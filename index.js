02a1b5c7c1f9a80244702f7044fffad5012d53cc
wuhy <sparklewhy@gmail.com>
2015-12-01 20:00:58 +0800

    init

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @file fisx-helper 模块
+ * @author sparklewhy@gmail.com
+ */
+
+var _ = require('lodash');
+var extractor = require('./lib/extractor');
+
+module.exports = exports = _.assign({}, extractor);
